8187a7617ff788d5c9d4ef64bf50602e5ea98cdb
Neema Velasco <neemavelasco@gmail.com>
2022-09-16 17:06:42 -0500

    created check sequence, update level, and reset game functions

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const buttons = document.querySelectorAll(".button");
+const level = document.querySelector(".level");
+const gameOver = document.querySelector(".game-over")
+const restartButton = document.querySelector(".restart-button");
+const color = document.querySelector(".color");
 
 const gameButtons = ["red", "green", "yellow", "blue", "orange", "purple"];
 
 let gameSequence = [];
+let playerSequence = [];
+let currentSequence = 0;
+let currentLevel = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -14,10 +21,17 @@ buttons.forEach((button) => {
     setTimeout(() => {
       button.classList.remove("pressed");
     }, 300);
+
+    playerSequence.push(playerMove)
+    checkSequence();
   })
 })
 
 const nextSequence = () => {
+  // clear player sequence
+  playerSequence = [];
+  currentSequence = 0;
+
   // generate random number to represent next move in sequence
   const move = randomNumber();
   gameSequence.push(gameButtons[move]);
@@ -27,6 +41,31 @@ const nextSequence = () => {
       animateButton(move);
     }, i * 1000)
   })
+}
+
+const checkSequence = () => {
+  if(gameSequence[currentSequence] === playerSequence[currentSequence]) {
+    if(currentSequence < gameSequence.length - 1 ){
+      currentSequence++
+    } else {
+      updateLevel();
+      setTimeout(() => {
+        nextSequence();
+      }, 1000)
+      
+    }
+  } else {
+    console.log("game over")
+    restartButton.style.visibility = "visible";
+    gameOver.style.visibility = "visible";
+  }
+  
+}
+
+const updateLevel = () => {
+  currentLevel++
+  console.log(currentLevel);
+  level.textContent = currentLevel;
 }
 
 const animateButton = (buttonId) => {
@@ -43,8 +82,26 @@ const randomNumber = () => {
 };
 
 const startGame = () => {
-  nextSequence();
+  resetGame();
 
+  setTimeout(() => {
+    nextSequence();
+  }, 2000);
 }
+
+const resetGame = () => {
+  currentLevel = 0;
+  currentSequence = 0;
+  playerSequence = [];
+  gameSequence = [];
+
+  level.textContent = currentLevel;
+  restartButton.style.visibility = "hidden";
+  gameOver.style.visibility = "hidden";
+}
+
+restartButton.addEventListener("click", () => {
+  startGame();
+});
 
 startGame();
